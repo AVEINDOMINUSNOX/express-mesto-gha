421b8378b65ab05dfb37968352d9c4df6fbad0a8
Shinkarenko Anton <AVEINDOMINUSNOX@yandex.ru>
2023-06-14 15:14:01 +0500

    13 fix: catch errors

--- controllers/users.js
+++ controllers/users.js
@@ -20,7 +20,7 @@ const getUser = (req, res) => {
     .orFail(new Error('NotFound'))
     .then((user) => res.send(user))
     .catch((err) => {
-      if (err.name === 'CastError') {
+      if (err.name === 'NotFound') {
         res.status(ERROR_CODE_DATA_NOT_FOUND).send({
           message: `Пользователь с указанным _id не найден, произошла ошибка: ${err.message}`,
           stack: err.stack,
